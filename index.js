e2d64efec1324e64107ce9ebc83bd6f34435e140
RENJITHPS10 <renjithpstech@gmail.com>
2024-08-02 19:45:08 +0530

    Added leading person

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 const para1 = document.getElementById('p1');
 const para2 = document.getElementById('p2');
+const leader = document.getElementById('lead')
 
 
-let count = 0;
-let updatecount = (i, p) => {
-    count += i
-    p.textContent = count
+let count1 = 0;
+let count2 = 0;
+let updatecount = (i, team) => {
+    if (team == 'home') {
+        count1 += i
+        para1.textContent = count1
+    } else if (team == 'guest') {
+        count2 += i
+        para2.textContent = count2
 
+    }
+updateleader()
+}
+
+let updateleader=()=>{
+    if(count1>count2){
+        leader.textContent='HOME IS LEADING'
+    }else if(count1<count2){
+        leader.textContent='GUEST IS LEADING'
+    }else{
+        leader.textContent='SCORES ARE TIED'
+    }
 }
 
 
-const plus1 = () => updatecount(1, para1)
-const plus2 = () => updatecount(2, para1);
-const plus3 = () => updatecount(3, para1);
-const p2plus1 = () => updatecount(1, para2);
-const p2plus2 = () => updatecount(2, para2);
-const p2plus3 = () => updatecount(3, para2);
+const plus1 = () => { updatecount(1, 'home') }
+const plus2 = () => updatecount(2, 'home');
+const plus3 = () => updatecount(3, 'home');
+const p2plus1 = () => updatecount(1, 'guest');
+const p2plus2 = () => updatecount(2, 'guest');
+const p2plus3 = () => updatecount(3, 'guest');
 const reset = () => {
-    count = 0
-    para1.textContent = count
-    para2.textContent = count
+    count1 = 0
+    count2 = 0
+    para1.textContent = count1
+    para2.textContent = count2
+    updateleader()
 }
 
 
